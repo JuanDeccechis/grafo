ce1dc8307621bae5ac4e817935a21fdacf5fa4e0
JuanDeccechis <juandeccechis@gmail.com>
2018-07-26 13:01:59 -0300

    grafo completo con valores random

--- grafo - GoJS/js.js
+++ grafo - GoJS/js.js
@@ -1,13 +1,28 @@
-
 /*
 https://www.youtube.com/watch?v=7cfHF7yAoJE&ab_channel=GoJSJavaScriptDiagrammingLibrary
-permite crear nodo = grupo, y definir nodos que se agreguen en el grupo
 */
 
 document.querySelector("#graficar").addEventListener("click", setObjetivosYGraficar);
 document.querySelector("#btn-perspectivas").addEventListener("click", setPerspectivas);
 let cantidadPerspectivas = 0;
 let objetivosPerspectivas = [];
+let cantidadLinks = 0;
+let objetivos = [];
+    let objetivo = {
+    	"perspectiva":  0,
+    	"id": "",
+    	"valor": 0,
+    	"imagen": "",
+    	"colorOBjetivo": ""
+    }
+let links = [];
+    let Link = {
+    	"origen": "",
+    	"to": "",
+    	"color": "",
+    	"valor": ""
+    }
+
 
 function setPerspectivas(){
     cantidadPerspectivas = document.querySelector("#perspectivas").value;
@@ -16,86 +31,169 @@ function setPerspectivas(){
         obji.setAttribute("id", "inputObjetivo" + i);
         obji.setAttribute("placeholder", "cantidad objetivos p"+ i);
         document.querySelector("#inputs-objetivos").appendChild(obji);
-    }
+    };
+    links = document.createElement("input"); 
+    links.setAttribute("id", "inputLinks");
+    links.setAttribute("placeholder", "cantidad links");
+    document.querySelector("#inputs-objetivos").appendChild(links);
 }
 
 function setObjetivosYGraficar(){
 	setObjetivos();
+	setLinks();
 	graficar();
 }
 
 function setObjetivos(){
+	let cantidad = 0;
 	for (let i = 0; i < cantidadPerspectivas; i++) {
         let cantidadObjetivos = document.querySelector("#inputObjetivo" + i).value;
         objetivosPerspectivas[i] = cantidadObjetivos;
+    	for (let j = 0; j < objetivosPerspectivas[i]; j++) {
+    		let objetivo = new Object();
+    		objetivo.perspectiva = i;
+    		objetivo.id =  i + " - " + j;
+    		objetivo.valor = Math.random() * 10;
+    		objetivo.tendencia = Math.random();
+    		objetivo.imagen = "";
+    		objetivo.colorObjetivo = "";
+    		objetivos[cantidad] = objetivo;
+    		cantidad++;
+    	}
+    
     }	
 }
 
+function getColorRandom() {
+  var letters = '0123456789ABCDEF';
+  var color = '#';
+  for (var i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
+}
+
+function setLinks(){
+	cantidadLinks = document.querySelector("#inputLinks").value;
+	let cantidad = 0;
+	for (let i = 0; i < cantidadPerspectivas; i++) {
+        let cantidadObjetivos = document.querySelector("#inputObjetivo" + i).value;
+        cantidad = cantidad + parseInt(cantidadObjetivos);
+    }
+	for(let i = 0; i < cantidadLinks; i++){
+		let link = new Object();
+		link.origen = objetivos[Math.floor(Math.random() * cantidad)].id;
+		link.destino =objetivos[Math.floor(Math.random() * cantidad)].id;
+		link.color = getColorRandom();
+		link.valor = Math.random() * 10;
+		links[i] = link;
+	}
+}
+
 function graficar(){
-    let perspectiva;
-    let texto;
-    let objetivoVacio;
-    let objetivo;
-
-    
-
     let $ = go.GraphObject.make;
 	let diagram = new go.Diagram("myDiagramDiv");
 	diagram.initialContentAligment = go.Spot.Center;
-
 
 	let nodeDataArray = [
 	];
     
 	let linkDataArray = [
-		{from: "objetivo 1 - 0", to: "objetivo 0 - 0", color: "red"}
 	];
 	diagram.model = new go.GraphLinksModel(nodeDataArray, linkDataArray);
-    /*goIntro();*/
 
     cantidadPerspectivas = document.querySelector("#perspectivas").value;
-    let widthTotal = 400;
-    let heightTotal = 383;
+    let widthTotal = 500;
+    let heightTotal = 700;
     let myheight = heightTotal/cantidadPerspectivas;
     let mySize = widthTotal +" " + myheight;
-    console.log(mySize);
-    let yOrigen = 0;
-    let posOrigen = 0 + " " + yOrigen;
-    let myColor ="";
+    /*let yOrigen = 0;
+    let posOrigen = 0 + " " + yOrigen;*/
+    let colorObjetivo ="";
     let sizeTotal = widthTotal + " " + heightTotal;
 
-    
+    let cantidad = 0;
+
     for (let i = 0; i < cantidadPerspectivas; i++) {
-    	/*if (i%2)
-    		myColor = "lightgray";
-    	else
-    		myColor = "gray";
-    		*/
-    	diagram.model.addNodeData({key: "perspectiva "+i, color: "lightgray", isGroup: true, size: mySize, loc: posOrigen, highlight: "blue"});
-    	let objYOrigen = yOrigen + 20;
+    		
+    	diagram.model.addNodeData({key: "perspectiva "+i, color: "lightgray", isGroup: true, size: mySize, /*loc: posOrigen,*/ highlight: "blue"});
+    	/*let objYOrigen = yOrigen + 20;
     	let objXOrigen = 0;
-    	let posObjetivo;
+    	let posObjetivo;*/
     	for (let j = 0; j < objetivosPerspectivas[i]; j++) {
-    		objXOrigen = objXOrigen + 20;
-    		posObjetivo = objXOrigen + " " + objYOrigen;
-    		diagram.model.addNodeData({key: "objetivo "+i+ " - " +j, color: "red", group: "perspectiva "+i, loc: new go.Point(objXOrigen.value, objYOrigen.value)});
+    		if (objetivos[cantidad].valor < 3.34){
+	    		objetivos[cantidad].colorObjetivo = "red";
+    		}
+	    	else{
+	    		if (objetivos[cantidad].valor < 6.67){
+	    			objetivos[cantidad].colorObjetivo = "yellow";
+	    		}
+	    		else{
+	    			objetivos[cantidad].colorObjetivo = "green";
+	    		}
+	    	}
+	    	
+	    	if(objetivos[cantidad].tendencia < 0.4)
+	    		objetivos[cantidad].imagen = "images/flecha abajo.png";
+	    	else
+	    		if(objetivos[cantidad].tendencia < 0.6)
+	    			objetivos[cantidad].imagen = "images/flecha izquierda.png";
+	    		else
+	    			objetivos[cantidad].imagen = "images/flecha arriba.png";
+	    	
+    		/*objXOrigen = objXOrigen + 20;
+    		posObjetivo = objXOrigen + " " + objYOrigen;*/
+
+    		diagram.model.addNodeData({key: "objetivo "+i+ " - " +j, color: objetivos[cantidad].colorObjetivo, group: "perspectiva "+i, imagen: objetivos[cantidad].imagen/*, loc: new go.Point(objXOrigen.value, objYOrigen.value)*/});
+    		cantidad++;
     	}
-    	yOrigen = yOrigen + myheight;
-    	posOrigen = 0 + " " + yOrigen; 
+    	/*yOrigen = yOrigen + myheight;
+    	posOrigen = 0 + " " + yOrigen; */
     }
 
+
+    for (let i = 0; i < cantidadLinks; i++)
+    	diagram.model.addLinkData(	{from: "objetivo " + links[i].origen, to: "objetivo " + links[i].destino, color: links[i].color, valor: Math.ceil(links[i].valor).toString()});
+
+
     diagram.nodeTemplate = 
-		$(go.Node, go.Panel.Auto,
-			 new go.Binding("location", "loc", go.Point.parse),
+		$(go.Node, "Spot", /*go.Panel.Auto*/
+			{selectionObjectName: "PH",
+	        locationObjectName: "PH",
+	        /*resizable: true,*/
+	        resizeObjectName: "PH",
+	    	deletable: false},
+			/*new go.Binding("location", "loc", go.Point.parse),*/
 			$(go.Shape,
-				{ figure: "RoundedRectangle"},
+				{ figure: "Ellipse",
+				desiredSize: new go.Size(120, 60)},
 				new go.Binding("fill", "color")/*,
 				new go.Binding("position", "20,20")*/
 				),
-			/*location: 50 0*/
+			/*location: 50 0,*/
 			$(go.TextBlock,
 				{margin: 3},
-				new go.Binding("text", "key")));
+				new go.Binding("text", "key")),
+
+			$(go.Picture,
+      			{desiredSize: new go.Size(25,25), alignment: new go.Spot(0.9,0)/*, alignmentFocus: go.Spot.TopRight*/},
+      			new go.Binding("source", "imagen")));
+
+	diagram.linkTemplate =
+	    $(go.Link,
+	    		{routing: go.Link.AvoidsNodes,
+	    		 corner: 10,
+	    		 curve: go.Link.JumpOver  /*go.Link.JumpOver*/},  // link route should avoid nodes
+	    	$(go.Shape,
+	    		{strokeWidth: 2},
+	    		new go.Binding("stroke", "color")
+	    		),
+	    	$(go.Shape, { toArrow: "Triangle", strokeWidth: 2}, 
+	    		new go.Binding("fill", "color"),
+	    		new go.Binding("stroke", "color")),
+	    	$(go.TextBlock, 
+	    		new go.Binding("text", "valor"), {segmentOffset: new go.Point(0,10)}) /*texto del link*/
+	    );
 
 	diagram.groupTemplate =
 	    $(go.Group, "Vertical",
@@ -103,25 +201,24 @@ function graficar(){
 		    selectionObjectName: "PH",
 	        locationObjectName: "PH",
 	        resizable: true,
-	        resizeObjectName: "PH" },
-	      new go.Binding("location", "loc", go.Point.parse).makeTwoWay(go.Point.stringify),
+	        resizeObjectName: "PH",
+	        deletable: false },
+	      /*new go.Binding("location", "loc", go.Point.parse).makeTwoWay(go.Point.stringify),*/
 	      $(go.Panel, "Auto",
 			
-	    	$(go.Shape,  // using a Shape instead of a Placeholder
+	    	$(go.Shape, 
 	        	{ name: "PH",
-	        	strokeWidth: 2 },
+	        	strokeWidth: 2,
+	        	figure: "RoundedRectangle"},
 	        	new go.Binding("fill", "color"),
 	        	new go.Binding("stroke", "highlight"),
 	        	new go.Binding("desiredSize", "size", go.Size.parse).makeTwoWay(go.Size.stringify),
 	    	),
 	    	$(go.Placeholder,    
                         { padding: 1 }),
-	    	$(go.TextBlock,         // group title
+	    	$(go.TextBlock, 
         		{ alignment: go.Spot.BottomCenter, font: "Bold 12pt Sans-Serif" },
         		new go.Binding("text", "key"))
-
-
 	    ));
-
 	
 }
